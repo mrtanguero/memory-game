67429294f99bbf79a60da8354ff6fe1a397f6013
Ivan Jovanović <mrtanguero@gmail.com>
2020-12-09 19:23:30 +0100

    Get almost everything to work

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
 "use strict";
+const game = document.querySelector(".game-container");
+const allBacksides = document.querySelectorAll(".card__side--back");
 
-console.log("Connected!");
+const state = {
+  player: 0,
+  score: [0, 0],
+  movesPlayed: 0,
+  randomArray: [],
+  currentMove: [],
+  cardsOpen: [],
+};
+
+// Kreira random niz duplih brojeva od 1-8
+const array = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+for (let i = 0; i < 16; i++) {
+  state.randomArray.push(
+    array.splice(Math.floor(Math.random() * array.length), 1)[0]
+  );
+}
+
+// Postavljanje fotki i data-value atributa koje koristim za poređenje
+state.randomArray.forEach((num, ind) => {
+  const image = document.createElement("img");
+  image.src = `img/${num}.png`;
+  allBacksides[ind].textContent = "";
+  allBacksides[ind].append(image);
+  allBacksides[ind].parentElement.setAttribute("data-value", num);
+});
+
+function reactToMove(e) {
+  const card = e.target.closest(".card");
+  if (!card) return;
+
+  card.querySelector(".card__side--front").classList.add("rotated");
+  card.querySelector(".card__side--back").classList.add("rotated");
+  state.movesPlayed++;
+  state.currentMove.push(card);
+  console.log(state.currentMove);
+
+  if (state.movesPlayed === 2) {
+    if (card === state.currentMove[0]) {
+      state.currentMove.pop();
+      console.log(state.currentMove);
+      state.movesPlayed--;
+      return;
+    }
+    game.removeEventListener("click", reactToMove);
+    state.movesPlayed = 0;
+    if (card.dataset.value === state.currentMove[0].dataset.value) {
+      state.cardsOpen.push(...state.currentMove);
+      state.currentMove = [];
+      state.score[state.player]++;
+      console.log(state.score);
+      game.addEventListener("click", reactToMove);
+    } else {
+      state.player = state.player ? 0 : 1;
+      console.log(state.player);
+      setTimeout(() => {
+        state.currentMove.forEach((c) => {
+          c.querySelector(".card__side--front").classList.remove("rotated");
+          c.querySelector(".card__side--back").classList.remove("rotated");
+          state.currentMove = [];
+          game.addEventListener("click", reactToMove);
+        });
+      }, 2000);
+    }
+  }
+}
+
+// Event listeners
+game.addEventListener("click", reactToMove);
